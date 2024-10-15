b972fbd8a0a9be67d5810b3f748b0c89081d57ab
M R Kishore Kumar <mrkishorekumar18@gmail.com>
2024-10-15 17:04:31 +0530

    save file functionality

--- components/PlaygroundTable.tsx
+++ components/PlaygroundTable.tsx
@@ -5,6 +5,21 @@ interface PlaygroundTableProp {
 }
 
 function PlaygroundTable({ setRenameModal }: PlaygroundTableProp) {
+  const saveFile = (
+    javascriptCode: string,
+    fileName: string,
+    lang: "js" | "ts",
+  ) => {
+    const file = `${fileName}.${lang}`;
+    const fileContent = javascriptCode;
+    const blob = new Blob([fileContent], { type: "text/javascript" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = file;
+    link.click();
+    URL.revokeObjectURL(link.href);
+  };
+
   return (
     <table className="w-full text-sm text-left rtl:text-right text-gray-500">
       <thead className="text-base text-white bg-headerBg sticky top-0 border-b-2 border-borderColor">
@@ -71,7 +86,7 @@ function PlaygroundTable({ setRenameModal }: PlaygroundTableProp) {
                 <path d="M680-80q-50 0-85-35t-35-85q0-6 3-28L282-392q-16 15-37 23.5t-45 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q24 0 45 8.5t37 23.5l281-164q-2-7-2.5-13.5T560-760q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-24 0-45-8.5T598-672L317-508q2 7 2.5 13.5t.5 14.5q0 8-.5 14.5T317-452l281 164q16-15 37-23.5t45-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-200q0-17-11.5-28.5T680-240q-17 0-28.5 11.5T640-200q0 17 11.5 28.5T680-160ZM200-440q17 0 28.5-11.5T240-480q0-17-11.5-28.5T200-520q-17 0-28.5 11.5T160-480q0 17 11.5 28.5T200-440Zm480-280q17 0 28.5-11.5T720-760q0-17-11.5-28.5T680-800q-17 0-28.5 11.5T640-760q0 17 11.5 28.5T680-720Zm0 520ZM200-480Zm480-280Z" />
               </svg>{" "}
             </button>
-            <button>
+            <button onClick={() => saveFile("console.log(1+1)", "main", "ts")}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="24px"
