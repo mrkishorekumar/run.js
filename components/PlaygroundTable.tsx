8f04f9f66212627e3008d3f63fed5073441bfe88
kishore.mr-v <kishore.mr-v@abfrl.adityabirla.com>
2024-10-17 21:41:25 +0530

    added ui for empty Playground

--- components/PlaygroundTable.tsx
+++ components/PlaygroundTable.tsx
@@ -20,6 +20,7 @@ interface PlaygroundTableProp {
   >;
   userCodeBaseData: UserCodeBase[];
   getUserCodebase: () => Promise<void>;
+  setCreateNewModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 function PlaygroundTable({
@@ -27,6 +28,7 @@ function PlaygroundTable({
   setShareModal,
   userCodeBaseData,
   getUserCodebase,
+  setCreateNewModal,
 }: PlaygroundTableProp) {
   const saveFile = (
     javascriptCode: string,
@@ -77,6 +79,42 @@ function PlaygroundTable({
       }
       starButtonRef.current.disabled = false;
     }
+  }
+
+  if (userCodeBaseData.length === 0) {
+    return (
+      <main className="w-full h-full flex flex-col justify-center items-center gap-7 pb-16">
+        <svg
+          className="animate-slow-bounce"
+          xmlns="http://www.w3.org/2000/svg"
+          height="100px"
+          viewBox="0 -960 960 960"
+          width="100px"
+          fill="#e8eaed"
+        >
+          <path d="M480-120q-151 0-255.5-46.5T120-280v-400q0-66 105.5-113T480-840q149 0 254.5 47T840-680v400q0 67-104.5 113.5T480-120Zm0-488q86 0 176.5-26.5T773-694q-27-32-117.5-59T480-780q-88 0-177 26t-117 60q28 35 116 60.5T480-608Zm-1 214q42 0 84-4.5t80.5-13.5q38.5-9 73.5-22t63-29v-155q-29 16-64 29t-74 22q-39 9-80 14t-83 5q-42 0-84-5t-80.5-14q-38.5-9-73-22T180-618v155q27 16 61 29t72.5 22q38.5 9 80.5 13.5t85 4.5Zm1 214q48 0 99-8.5t93.5-22.5q42.5-14 72-31t35.5-35v-125q-28 16-63 28.5T643.5-352q-38.5 9-80 13.5T479-334q-43 0-85-4.5T313.5-352q-38.5-9-72.5-21.5T180-402v126q5 17 34 34.5t72 31q43 13.5 94 22t100 8.5Z" />
+        </svg>
+        <h1 className="font-sans font-bold text-xl text text-white">
+          Your playground is currently empty. Start building something awesome!
+          🔥
+        </h1>
+        <button
+          className="bg-blueBtn flex items-center bg-blue-500 text-white p-2 rounded-md shadow gap-3"
+          onClick={() => setCreateNewModal(true)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#FFFFFF"
+          >
+            <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
+          Create Playground!
+        </button>
+      </main>
+    );
   }
 
   return (
--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -109,46 +109,51 @@ function Playgrounds() {
           Coding playgrounds on RunJs are powered by VS Code IDE and start
           within a few seconds. Practice coding while learning for free.
         </p>
-        <div className="flex items-center justify-between mb-4">
-          <div className="flex items-center w-1/2 bg-headerBg p-2 rounded-md shadow">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#FFFFFF"
+        {userCodeBaseData.length > 0 ? (
+          <div className="flex items-center justify-between mb-4">
+            <div className="flex items-center w-1/2 bg-headerBg p-2 rounded-md shadow">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#FFFFFF"
+              >
+                <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+              </svg>
+              <input
+                type="text"
+                placeholder="Search files"
+                className="w-full bg-transparent outline-none ml-3 text-white"
+                onChange={(e) => debouncedFilterSearchTerm(e.target.value)}
+              />
+            </div>
+            <button
+              className="bg-blueBtn flex items-center bg-blue-500 text-white p-2 rounded-md shadow gap-3"
+              onClick={() => setCreateNewModal(true)}
             >
-              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-            </svg>
-            <input
-              type="text"
-              placeholder="Search files"
-              className="w-full bg-transparent outline-none ml-3 text-white"
-              onChange={(e) => debouncedFilterSearchTerm(e.target.value)}
-            />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#FFFFFF"
+              >
+                <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+              </svg>
+              Create Playground!
+            </button>
           </div>
-          <button
-            className="bg-blueBtn flex items-center bg-blue-500 text-white p-2 rounded-md shadow gap-3"
-            onClick={() => setCreateNewModal(true)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#FFFFFF"
-            >
-              <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-            </svg>
-            Create Playground!
-          </button>
-        </div>
-        <section className="h-3/4 w-full bg-headerBg overflow-auto mt-2 rounded">
+        ) : null}
+        <section
+          className={`h-3/4 w-full bg-headerBg overflow-auto mt-2 rounded ${userCodeBaseData.length === 0 ? "mt-10" : ""}`}
+        >
           <PlaygroundTable
             setRenameModal={setRenameModal}
             setShareModal={setShareModal}
             userCodeBaseData={filterData}
             getUserCodebase={getUserCodebase}
+            setCreateNewModal={setCreateNewModal}
           />
         </section>
       </main>
