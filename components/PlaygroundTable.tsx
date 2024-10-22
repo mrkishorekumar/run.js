044462bce68ac70185c80953fc2cb8f3c80390fa
M R Kishore Kumar <mrkishorekumar18@gmail.com>
2024-10-22 21:56:29 +0530

    added Recently Deleted btn when playground is empty

--- components/PlaygroundTable.tsx
+++ components/PlaygroundTable.tsx
@@ -100,21 +100,38 @@ function PlaygroundTable({
           Your playground is currently empty. Start building something awesome!
           🔥
         </h1>
-        <button
-          className="bg-blueBtn flex items-center bg-blue-500 text-white p-2 rounded-md shadow gap-3"
-          onClick={() => setCreateNewModal(true)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#FFFFFF"
+        <div className="flex gap-8">
+          <button
+            className="bg-blueBtn flex items-center bg-blue-500 text-white p-2 rounded-md shadow gap-3"
+            onClick={() => setCreateNewModal(true)}
           >
-            <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-          </svg>
-          Create Playground!
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#FFFFFF"
+            >
+              <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+            </svg>
+            Create Playground!
+          </button>
+          <button
+            className="bg-transparent flex items-center text-white p-2 rounded-md gap-3"
+            onClick={() => router.push("bin")}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#FFFFFF"
+            >
+              <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+            </svg>
+            Recently Deleted
+          </button>
+        </div>
       </main>
     );
   }
